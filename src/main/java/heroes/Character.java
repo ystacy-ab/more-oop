f3e84b3b551d7a33725c41ba924ac97444768f8b
Anastasiia Yablunovska <yablunovska.pn@ucu.edu.ua>
2025-10-04 22:58:33 +0300

    added tests

--- src/main/java/heroes/Character.java
+++ src/main/java/heroes/Character.java
@@ -16,6 +16,10 @@ public abstract class Character {
         return hp;
     }
 
+    public int getPower() {
+        return power;
+    }
+
     public abstract void kick(Character c);
 
     @Override
